ddad9eaf00a0c1a53a48624629e0e344e105dc12
Buu Pham <pphambuu@gmail.com>
2022-09-23 22:19:33 +0700

    add difficulty

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from pygame.locals import *
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (200, 0, 0)
+GREEN = (0, 200, 0)
+YELLOW = (255, 191, 0)
+ROBIN_BLUE = (31, 206, 203)
 WIDTH = 600
 HEIGHT = 600
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -85,17 +88,47 @@ class Grid:
 
     def check_keyboard(self):
         keyboard = pygame.key.get_pressed()
-        if keyboard[K_RIGHT] and self.snake.direction!='left':
+        if keyboard[K_RIGHT] and self.snake.direction != 'left':
             self.snake.direction = 'right'
-        elif keyboard[K_LEFT] and self.snake.direction!='right':
+        elif keyboard[K_LEFT] and self.snake.direction != 'right':
             self.snake.direction = 'left'
-        elif keyboard[K_UP] and self.snake.direction!='down':
+        elif keyboard[K_UP] and self.snake.direction != 'down':
             self.snake.direction = 'up'
-        elif keyboard[K_DOWN] and self.snake.direction!='up':
+        elif keyboard[K_DOWN] and self.snake.direction != 'up':
             self.snake.direction = 'down'
 
 
 grid = Grid(30)
+
+
+def create_text(text, x, y, fontSize=20):
+    mytext = pygame.font.Font("freesansbold.ttf", fontSize)
+    textSurface = mytext.render(text, True, BLACK)
+    textRect = textSurface.get_rect()
+    textRect.center = (x, y)
+    SCREEN.blit(textSurface, textRect)
+
+
+def create_button(msg, x, y, w, h, color, colorOnHover, action=None):
+    myRect = pygame.Rect(x, y, w, h)
+    pygame.draw.rect(SCREEN, color, myRect)
+    centerx, centery = myRect.center
+    create_text(msg, centerx, centery, 30)
+    mousex, mousey = pygame.mouse.get_pos()
+
+    if x < mousex < x + w and y < mousey < y + h:
+        pygame.draw.rect(SCREEN, colorOnHover, myRect)
+        press = pygame.mouse.get_pressed()
+        if press[0] == 1 and action != None:
+            if msg == "Easy":
+                FPS = 10
+                action(FPS)
+            elif msg == "Medium":
+                FPS = 20
+                action(FPS)
+            else:
+                FPS = 30
+                action(FPS)
 
 
 def check_quit():
@@ -105,7 +138,22 @@ def check_quit():
             sys.exit()
 
 
-def main_game_loop():
+def difficulty():
+    bg=pygame.transform.scale(pygame.image.load('bg.jfif'),(WIDTH,HEIGHT))
+    while True:
+        SCREEN.fill(WHITE)
+        SCREEN.blit(bg,(0,0))
+        create_button("Easy", WIDTH // 2 - 50, 100, 150, 50, GREEN, ROBIN_BLUE, main_game_loop)
+        create_button("Medium", WIDTH // 2 - 50, 300, 150, 50, YELLOW, ROBIN_BLUE, main_game_loop)
+        create_button("Hard", WIDTH // 2 - 50, 500, 150, 50, RED, ROBIN_BLUE, main_game_loop)
+        pygame.display.update()
+        check_quit()
+
+        pygame.display.update()
+        CLOCK.tick(30)
+
+
+def main_game_loop(FPS):
     game = True
     while game:
         SCREEN.fill(BLACK)
@@ -117,7 +165,7 @@ def main_game_loop():
         grid.draw_snake()
         grid.generate_food()
         pygame.display.update()
-        CLOCK.tick(15)
+        CLOCK.tick(FPS)
 
 
-main_game_loop()
+difficulty()
